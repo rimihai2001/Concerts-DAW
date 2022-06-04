7ceb9f7784c6b7f8382ae8b054c37f1fc6799656
rimihai2001 <rimihai2001@gmail.com>
2022-06-04 18:30:14 +0300

    Login Frontend Done

--- Frontend/FrontendDAW/src/app/modules/auth/auth.module.ts
+++ Frontend/FrontendDAW/src/app/modules/auth/auth.module.ts
@@ -4,9 +4,15 @@ import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './login/login.component';
 import { MaterialModule } from '../material/material.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [LoginComponent],
-  imports: [CommonModule, AuthRoutingModule, MaterialModule],
+  imports: [
+    CommonModule,
+    AuthRoutingModule,
+    MaterialModule,
+    ReactiveFormsModule,
+  ],
 })
 export class AuthModule {}
--- Frontend/FrontendDAW/src/app/modules/auth/login/login.component.ts
+++ Frontend/FrontendDAW/src/app/modules/auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
@@ -7,7 +8,19 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+  public loginForm: FormGroup = new FormGroup({
+    username: new FormControl(''),
+    password: new FormControl(''),
+  });
   constructor(private router: Router) {}
+
+  get username(): AbstractControl {
+    return this.loginForm.get('username');
+  }
+
+  get password(): AbstractControl {
+    return this.loginForm.get('password');
+  }
 
   ngOnInit(): void {}
 
